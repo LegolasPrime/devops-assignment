492ce2f384a5ba06fc0a88e32018e440535bd550
LegolasJunior <jaiveersingh016@gmail.com>
2021-10-10 21:37:39 +0530

    Development feature added.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World! Initial Commit!";
+  cout << "New edit by collaborator.";
+  cout << "New edit by manager.";
+  cout << "Added new feature.";
+  cout << "Development feature added.";
+  return 0;
+}
