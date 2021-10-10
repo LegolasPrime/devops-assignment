a78da3f2104157bca20e305f84477e98da0208e7
LegolasJunior <jaiveersingh016@gmail.com>
2021-10-10 20:48:29 +0530

    Conflict Resolution New

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@ using namespace std;
 int main()
 {
   cout << "Hello World! Initial Commit!";
-<<<<<<< HEAD
   cout << "New edit by collaborator.";
-=======
   cout << "New edit by manager.";
->>>>>>> 01fa3ec623af8af288af454566ec8023955a2256
   return 0;
 }
